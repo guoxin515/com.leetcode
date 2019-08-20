6fd703712f7a77d6daaf07dc6134283423c34e04
郭鑫 <guoxin1@u51.com>
2019-08-20 09:36:53 +0800

    8

---
+++ src/mianshizhinan_166/stack_queue/util/Node.java
@@ -0,0 +1,20 @@
+package mianshizhinan_166.stack_queue.util;
+
+/**
+ * @author guoxin
+ * @version 2019/8/20
+ *
+ * 定义二叉树节点
+ */
+public class Node {
+
+    public int value;
+
+    public Node left;
+
+    public Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
